da62196ec14c7563400e49906fee38b713b4e0f0
seonghyeon cho <ghssh1@tistory.com>
2017-03-02 21:19:19 +0900

    170302 수정1

--- input/CSHWritableMK2.scala
+++ input/CSHWritableMK2.scala
@@ -2,6 +2,11 @@
  *	2017/03/01
  *	읽을 때는 PersonRecord 단위로 읽고
  *	이후 Reduce를 통해 이를 CSHWritableMK2로 변환
+ *
+ *	2017/03/02
+ *	(이름+type, PersonRecord)
+ *	--> Reduce
+ *	--> (이름+type, Writable)
  */
 
 package org.apache.spark.input
@@ -13,8 +18,8 @@ import java.io.{DataInput, DataOutput}
  *	기본 읽는 단위.
  */
 class PersonRecord {
-	var name: String = _
-	var actionType: Long = _
+	// var name: String = _
+	// var actionType: Long = _
 	var timestamp: Long = _
 
 	//coords
@@ -23,20 +28,38 @@ class PersonRecord {
 
 /**
  *	Writable wrapper for Person records
- *	reduce (Key, Value) = (이름+ActionType, CSHWritableMK2)
+ *	reduce (Key, Value) = (이름+actionType, CSHWritableMK2)
  */
 class CSHWritableMK2
 	extends WritableComparable[CSHWritableMK2] {
+
+	private var name = ""
+	private var actionType = 0L
+	private var record = new PersonRecord
 
 	/**
 	 *	override
 	 */
 	override def readFields(in: DataInput): Unit = {
+		name = in.readUTF
+		actionType = in.readLong
+		record.timestamp = in.readLong
 
+		val numOfCoords = in.readLong
+		for (i <- 1 to numOfCoords) {
+			record.coordinates += in.readDouble
+		}
 	}
 
 	override def write(out: DataOutput): Unit = {
+		writeUTF(name)
+		writeLong(actionType)
+		out.writeLong(record.timestamp)
 
+		out.writeInt(record.coordinates.size)
+		record.coordinates.foreach(coord => {
+			out.writeDouble(coord)
+		})
 	}
 
 	override def compareTo(other: CSHWritableMK2): Int = {
@@ -44,7 +67,7 @@ class CSHWritableMK2
 	}
 
 	override def toString(): String = {
-		s""
+		s"name: ${name}, type: ${actionType}"
 	}
 }
 
@@ -52,14 +75,52 @@ class CSHWritableMK2
  *	Person Record Reader
  */
 class CSHPersonRecordReader
-	extends RecordReader[String, ???] {
+	extends RecordReader[String, CSHWritableMK2] {
+
+	private var splitStart = 0L
+	private var splitEnd = 0L
+	private var currentPosition = 0L
+
+	private var fileInputStream: FSDataInputStream = null
+	private var recordKey = ""
+	private var recordValue = new PersonRecord
+
+
+	/**
+	 *	override
+	 */
+	override def close(): Unit = {
+		if (fileInputStream != null) {
+			fileInputStream.close
+		}
+	}
+
+	override def getCurrentKey() = recordKey
+
+	override def getCurrentValue() = recordValue
+
+	override def getProgress() = {
+
+	}
+
+	override def initialize(inputSplit: InputSplit,
+		context: TaskAttemptContext) = {
+
+	}
+
+	override def nextKeyValue(): Boolean = {
+		/**
+		 *	Update recordKey, recordValue
+		 */
+		false
+	}
 }
 
 /**
  *	sc.newAPIHadoopFile 등에 넣을 InputFormat
  */
 class CSHPersonRecordInputFormat
-	extends FileInputFormat[String, ???] {
+	extends FileInputFormat[String, CSHWritableMK2] {
 	/**
 	 *	override
 	 */
